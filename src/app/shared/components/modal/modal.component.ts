46431f8ae1d0d96dfbe9cde9e723b488b55cee58
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-10 22:18:45 -0300

    finalizando o css do overlay

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostBinding, AfterViewInit } from '@angular/core';
+
 import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['modal.component.scss']
+  styleUrls: ['modal.component.scss'],
 })
 export class ModalComponent {
   public config: ModalConfig;
