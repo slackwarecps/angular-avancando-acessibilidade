e2fe0fbd665fbb09cafab5c0960eff00deb0e1dd
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-10 21:38:32 -0300

    view implementada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,35 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, TemplateRef, ViewChild } from '@angular/core';
 import { ModalRef, ModalService } from './shared/components/modal/services/modal.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
+
+  @ViewChild('template1') public template1: TemplateRef<any>;
+  @ViewChild('template2') public template2: TemplateRef<any>;
+  public selectedTemplate: TemplateRef<any>;
+
   title = 'a11y-p2';
   public firstName = 'Flávio';
   public modalRef: ModalRef;
 
-  constructor(private modalService: ModalService) {}
+  constructor(private modalService: ModalService, private cd: ChangeDetectorRef) {}
+
+  public ngAfterViewInit(): void {
+    this.selectedTemplate = this.template1;
+    this.cd.detectChanges();
+  }
 
   public show(): void {
+    this.selectedTemplate = this.template2;
+
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
-      title: 'User Details'
+      title: 'User Details',
     });
   }
 }
--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,22 +1,35 @@
-import { TemplateRef } from '@angular/core';
+import { BodyInjectorService } from './../../../../body-injectot';
+import { ModalComponent } from './../modal.component';
+import { ComponentFactory, ComponentFactoryResolver, Injector, TemplateRef } from '@angular/core';
 import { Injectable } from '@angular/core';
+import { ModalConfig } from '../interfaces/modal-config';
 
 @Injectable()
 export class ModalService {
+  private componenteFactory: ComponentFactory<ModalComponent>;
+
+  constructor(
+    componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    private bodyInjector: BodyInjectorService
+  ) {
+    componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
 
   public open(config: ModalConfig): ModalRef {
+    const componentRef = this.createComponentRef(this.injector);
+    componentRef.instance.config = config;
+    console.log(componentRef.instance);
     console.log('open called');
-    return new ModalRef();
+    this.bodyInjector.stackBeforeAppRoot(componentRef);
+    return new ModalRef(componentRef);
+  }
+  createComponentRef(injector: Injector) {
+    return this.componenteFactory.create(this.componenteFactory);
   }
 }
 
-export interface ModalConfig {
-  templateRef: TemplateRef<any>;
-  title: string;
-}
-
 export class ModalRef {
-
   public close(): void {
     console.log('close called');
   }
