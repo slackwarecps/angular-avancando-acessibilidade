7498d97f531fe46b47f4d8f23a89b27348ff67ec
‘fabioalvaro’ <fabio.alvaro@gmail.com>
2022-05-12 16:42:12 -0300

    fim da lapidacao do componente

--- README.md
+++ README.md
@@ -18,3 +18,9 @@ Como minimizar dependências de um Componente
 A diretiva \*ngTemplateOutlet
 ComponentFactoryResolver para criar fábricas de Componentes
 O papel de ApplicationRef e HotView de um ComponentRef
+
+#
+
+Estilização padrão de um modal
+Animações com o módulo Animations
+Como reutilizar animações
--- src/app/shared/components/modal/services/modal.service.ts
+++ src/app/shared/components/modal/services/modal.service.ts
@@ -1,7 +1,9 @@
 import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector } from '@angular/core';
+
 import { BodyInjectorService } from 'src/app/shared/services/body-injector';
 import { ModalConfig } from '../interfaces/modal-config';
 import { ModalComponent } from '../modal.component';
+import { ModalRef } from '../models/modal-ref';
 
 @Injectable()
 export class ModalService {
@@ -22,20 +24,12 @@ export class ModalService {
     console.log(componentRef.instance);
     console.log('open called');
     this.bodyInjector.stackBeforeAppRoot(componentRef);
-    return new ModalRef(componentRef);
+    const modalRef = new ModalRef(componentRef);
+    componentRef.instance.modalRef = modalRef;
+    return modalRef;
   }
 
   private createComponentRef(): ComponentRef<ModalComponent> {
     return this.componentFactory.create(this.injector);
-  }
-}
-
-export class ModalRef {
-
-  constructor(private componentRef: ComponentRef<ModalComponent>) {}
-
-  public close(): void {
-    console.log('close called');
-    this.componentRef.destroy();
   }
 }
